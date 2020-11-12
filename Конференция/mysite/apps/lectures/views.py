ab72d06a2313449ed5f285e6daa55fe520093f46
Arseny Smirnov <Arseny.Smirnov@urfu.me>
2020-11-12 20:49:11 +0500

    скрипт заполнения(не работает)

--- Конференция/mysite/apps/lectures/views.py
+++ Конференция/mysite/apps/lectures/views.py
@@ -1,10 +1,31 @@
+import time
+
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.utils import timezone
+
 from .models import Lecture
+from random import random
 
 
 def index(request):
+    # _audience_number = [1, 2, 3, 4, 5, 6, 7]
+    # _lecture_title = ['Математика', 'Русский язык', 'Английский язык', 'Литература', 'Музыка', 'Кино', 'Спорт']
+    # _speaker_name = ['Павел', 'Андрей', 'Артем', 'Константин', 'Мария', ]
+    # _description = 'Какое то описание'
+    # _start_time = timezone.now()
+    # _end_time = timezone.now()
+    # for i in range(0, 5):
+    #     a = Lecture(audience_number=random.choice(_audience_number),
+    #                 lecture_title=random.choice(_lecture_title),
+    #                 speaker_name=random.choice(_speaker_name),
+    #                 description=_description,
+    #                 start_time=_start_time,
+    #                 end_time=_end_time)
+    #
+    #     a.save()
+
     lectures_list = Lecture.objects.order_by('start_time')
 
     unsorted_audiences_list = Lecture.objects.values_list('audience_number', flat=True)
